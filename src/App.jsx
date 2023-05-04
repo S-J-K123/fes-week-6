5be67d6b430b4a0b46e8b824553adbb17c4cc7ea
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2023-05-04 20:17:11 +0100

    new

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+       <section>
+                <nav>
+                   <a href="">
+                   <i className="fa-solid fa-clapperboard">
+                      <FontAwesomeIcon icon="fa-solid fa-clapperboard" />
+                   </i>
+                 
+                   </a>
+                </nav>
+            </section>
+    </div>
+  );
+}
+
+export default App;
