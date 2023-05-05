bb8178ef699c41516b71e2052808155828ef457a
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2023-05-05 14:40:10 +0100

    hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './App.css';
+import { faClapperboard } from "@fortawesome/free-solid-svg-icons";
 
 
 function App() {
@@ -7,12 +8,17 @@ function App() {
     <div>
        <section>
                 <nav>
-                   <a href="">
-                   <i className="fa-solid fa-clapperboard">
-                      <FontAwesomeIcon icon="fa-solid fa-clapperboard" />
-                   </i>
-                 
-                   </a>
+            <a href="">
+              <div className="logo-img">
+              <FontAwesomeIcon className="icon" icon={faClapperboard} />
+                <h2 className="logo">movies</h2>
+              </div>
+            </a>
+                   <ul>
+                    <li className="lists"><a href=""><span className="home">Home</span> </a></li>
+                    <li className="lists"><a href="">Browse Movies</a></li>
+                    <li className="lists"><a href="">Favourites</a></li>
+                   </ul>
                 </nav>
             </section>
     </div>
