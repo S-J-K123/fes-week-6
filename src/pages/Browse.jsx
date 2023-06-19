3159ba4932e1409b4b5a9180d8705c6752a298be
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2023-06-20 00:22:58 +0100

    row

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -3,101 +3,154 @@ import {
   faMagnifyingGlass,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
 import Switch from "../components/Switch";
 import Modal from "../components/Modal";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Skeleton from "../UI/Skeleton";
 
 const Browse = () => {
   const [showModal, setShowModal] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [searchName, setSearchName] = useState("");
+  const [loading, setLoading] = useState(true);
 
+  function onSearch(event) {
+    event.preventDefault();
+    setLoading(false)
+    fetchUsers(searchName);
+    console.log(searchName);
+  }
 
+  async function fetchUsers(movieName) {
+    setLoading(true)
+    if (!movieName) {
+      setUsers([]);
+      return;
+    }
 
-  
+    const { data } = await axios.get(
+      `https://www.omdbapi.com/?i=tt3896198&apikey=8e3ddd4c&s=${movieName}`
+    );
+
+    setUsers(data.Search);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+    console.log(data);
+  }
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const movieName = searchParams.get("search");
+    fetchUsers(movieName);
+  }, []);
+
   return (
     <div>
-      <section className="bg-orange">
-        <nav className="bg-orange">
-          <a href="/">
-            <div className="logo-img-browse bg-orange ">
-              <FontAwesomeIcon
-                className="icon-browse bg-orange"
-                icon={faClapperboard}
-              />
-              <h2 className="logo-browse bg-orange">movies</h2>
-            </div>
-          </a>
-          <ul>
-            <li className="lists-browse bg-orange">
-              <a className="bg-orange-list" href="/">
-                {" "}
-                Home{" "}
-              </a>
-            </li>
-            <li className="lists-browse bg-orange">
-              <a className="bg-orange-list" href="browse">
-                Browse Movies
-              </a>
-            </li>
-            <li className="lists-browse bg-orange">
-              <a className="bg-orange-list" href="favourites">
-                Favourites
-              </a>
-            </li>
-          </ul>
-        </nav>
+      <div className="browse-background-color">
+        <section className="bg-orange">
+          <nav className="bg-orange">
+            <a href="/">
+              <div className="logo-img-browse bg-orange ">
+                <FontAwesomeIcon
+                  className="icon-browse bg-orange"
+                  icon={faClapperboard}
+                />
+                <h2 className="logo-browse bg-orange">movies</h2>
+              </div>
+            </a>
+            <ul>
+              <li className="lists-browse bg-orange">
+                <a className="bg-orange-list" href="/">
+                  {" "}
+                  Home{" "}
+                </a>
+              </li>
+              <li className="lists-browse bg-orange">
+                <a className="bg-orange-list" href="browse">
+                  Browse Movies
+                </a>
+              </li>
+              <li className="lists-browse bg-orange">
+                <a className="bg-orange-list" href="favourites">
+                  Favourites
+                </a>
+              </li>
+            </ul>
+          </nav>
 
-        <h1 className="bm bg-orange">Browse Our Movies</h1>
+          <h1 className="bm bg-orange">Browse Our Movies</h1>
 
-        <div className="search-container-browse bg-orange">
-          <form action="" className="bg-orange">
-            <input
-              className="browse-input"
-              type="text"
-              placeholder="Search thousands of movies..."
-              name="Search"
-            />
-            <button className="button-browse">
-              <FontAwesomeIcon
-                className="magnify-browse"
-                icon={faMagnifyingGlass}
+          <div className="search-container-browse bg-orange">
+            <form onSubmit={onSearch} className="bg-orange">
+              <input
+                value={searchName}
+                onChange={(event) => setSearchName(event.target.value)}
+                className="browse-input"
+                type="text"
+                placeholder="Search thousands of movies..."
+                name="Search"
               />
-            </button>
-          </form>
-        </div>
-      </section>
+              <button className="button-browse">
+                <FontAwesomeIcon
+                  className="magnify-browse"
+                  icon={faMagnifyingGlass}
+                />
+              </button>
+            </form>
+          </div>
+        </section>
+      </div>
+
+      {/* changed from swich-btn to switch-input */}
 
       <section className="btm-half">
         <h2 className="search-results">Search results</h2>
         <h2 className="filter">Filter by year</h2>
-       <button className="switch-btn"   onClick={()=>setShowModal(!showModal)}> <Switch/> </button> 
-       
+        <button className="switch-btn" onClick={() => setShowModal(!showModal)}>
+          {" "}
+          <Switch />{" "}
+        </button>
       </section>
 
-      <div className="row">
-        <div className="user-list">
-          <div className="user">
-            <div className="user-card">
-              <div className="user-card__container">
-                <h3>
-                  Poster: <b>Poster</b>{" "}
-                </h3>
-                <p>
-                  Title: <b>Title</b>{" "}
-                </p>
-                <p>
-                  Type: <b>Type</b>
-                </p>
-                <p>
-                  Year: <b>Year</b>
-                </p>
-              </div>
+      {showModal ? <Modal /> : null}
+
+      {users && users.map((user, id) => {
+        return (
+          <div className="row" key={id}>
+            <div className="user-list">
+             
+                  <div className="user">
+                    {
+                      loading
+                      ? (
+                        <Skeleton width={'300px'} height={'300px'}/>
+                      ) :
+                      (
+                        <div className="user-card">
+                        <div className="user-card__container">
+                          <img className="images" src={user.Poster} alt="" />{" "}
+                          <p>
+                            Title: <b>{user.Title}</b>{" "}
+                          </p>
+                          <p>
+                            Type: <b>{user.Type}</b>
+                          </p>
+                          <p>
+                            Year: <b>{user.Year}</b>
+                          </p>
+                        </div>
+                      </div>
+                      )
+                    }
+                
+                </div>
+             
             </div>
           </div>
-        </div>
-      </div>
-
-      {showModal ? <Modal /> : null}
-    
+        );
+      })}
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,112 @@
-import { faClapperboard, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import {
+  faClapperboard,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
+import { navigate, useNavigate } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Home = () => {
-    return (
-        <div>
-               <section className="home-page">
-                <nav>
-            <a href="/">
-              <div className="logo-img">
+  let navigate = useNavigate();
+  const [searchName, setSearchName] = useState("");
+  const [loading, setLoading] = useState(false)
+
+  function onSearch() {
+    setLoading(true)
+    navigate(`/browse?search=${searchName}`);
+    console.log(searchName);
+    
+  }
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  return (
+    <div>
+      <section className="home-page">
+        <nav>
+          <a href="/">
+            <div className="logo-img">
               <FontAwesomeIcon className="icon" icon={faClapperboard} />
-                <h2 className="logo">movies</h2>
-              </div>
-            </a>
-                   <ul>
-                    <li className="lists"><a href="/"><span className="org-li">Home</span> </a></li>
-                    <li className="lists"><a href="browse">Browse Movies</a></li>
-                    <li className="lists"><a href="favourites">Favourites</a></li>
-                   </ul>
-                </nav>
+              <h2 className="logo">movies</h2>
+            </div>
+          </a>
+          <ul>
+            <li className="lists">
+              <a href="/">
+                <span className="org-li">Home</span>{" "}
+              </a>
+            </li>
+            <li className="lists">
+              <a href="browse">Browse Movies</a>
+            </li>
+            <li className="lists">
+              <a href="favourites">Favourites</a>
+            </li>
+          </ul>
+        </nav>
 
+        <h1
+          data-aos="fade-up"
+          data-aos-delay="50"
+          data-aos-duration="1000"
+          className="big-text"
+        >
+          Britain's most awarded <br />
+          free movie platform
+        </h1>
 
-                <h1 className="big-text">
-                  Britain's most awarded <br />
-                  free movie platform
-                </h1>
+        <h2
+          data-aos="fade-up"
+          data-aos-delay="50"
+          data-aos-duration="1500"
+          className="fav"
+        >
+          find your Favourite <span className="shows">movies and shows</span>
+        </h2>
 
-                <h2 className="fav">
-                  find your Favourite <span className="shows">movies and shows</span>
-                </h2>
-
-
-                <div className="search-container">
-                <form action="">
-                  <input type="text"  placeholder="Search thousands of movies..." name="Search"/>
-                  <button className="button"><FontAwesomeIcon className="magnify" icon={faMagnifyingGlass} /></button>
-                </form>
-                </div>
-
-
-                <div className="footer-img" >
-                    <img className="btm-img" src="https://movies-au-react.vercel.app/static/media/undraw_explore_re_8l4v.0e6b1c627148e0f0c295c8c3dbfde077.svg" alt="" />
-                </div>
-            </section>
+        <div className="search-container">
+          <form action="" onSubmit={onSearch}>
+            <input
+              data-aos="fade-up"
+              data-aos-delay="50"
+              data-aos-duration="2000"
+              className="home-input"
+              onChange={(event) => setSearchName(event.target.value)}
+              value={searchName}
+              type="text"
+              placeholder="Search thousands of movies..."
+              name="Search"
+            />
+            <button
+              data-aos="fade-up"
+              data-aos-delay="50"
+              data-aos-duration="2500"
+              className="button"
+            >
+              <FontAwesomeIcon className="magnify" icon={faMagnifyingGlass} />
+            </button>
+          </form>
         </div>
-    );
-}
+
+        <div
+          data-aos="fade-up"
+          data-aos-delay="50"
+          data-aos-duration="2500"
+          className="footer-img"
+        >
+          <img
+            className="btm-img"
+            src="https://movies-au-react.vercel.app/static/media/undraw_explore_re_8l4v.0e6b1c627148e0f0c295c8c3dbfde077.svg"
+            alt=""
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Home;
